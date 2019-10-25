88171dc3a3dd19c56af05aa6c6419799832ddc46
jeh_dias <jeh_dias18@hotmail.com>
2019-10-25 18:16:46 -0300

    correcao de tipo de campo

--- MockRegister/MockRegister.Domain/Entity/Register.cs
+++ MockRegister/MockRegister.Domain/Entity/Register.cs
@@ -8,6 +8,6 @@ namespace MockRegister.Domain.Entity
     public class Register
     {
         public string PaymentMethod { get; set; }
-        public double Value { get; set; }
+        public int Value { get; set; }
     }
 }
